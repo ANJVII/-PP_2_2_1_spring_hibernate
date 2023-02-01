b1655fe46dcfbea336a7ef767844dd2e2a0f31b3
Anjela_MV <m.angel.v@mail.ru>
2023-02-02 00:02:02 +0800

    - v 1.0 :)

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -1,10 +1,12 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -17,6 +19,15 @@ public class UserDaoImp implements UserDao {
    @Override
    public void add(User user) {
       sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   public User getCarsOwner(String model, int series) {
+      Query query = sessionFactory.getCurrentSession().createQuery("FROM Car where model = :model and series = :series");
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      List<Car> cars = query.getResultList();
+      return cars.get(0).getUser();
    }
 
    @Override
--- src/main/java/hiber/service/UserServiceImp.java
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -20,6 +20,12 @@ public class UserServiceImp implements UserService {
       userDao.add(user);
    }
 
+   @Transactional
+   @Override
+   public User getCarOwner(String model, int series) {
+      return userDao.getCarsOwner(model, series);
+   }
+
    @Transactional(readOnly = true)
    @Override
    public List<User> listUsers() {
